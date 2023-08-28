52d75c870612025fc660b6a46fccf8460cbfaa7c
Maria Clara Cunha <maria.clara@jusbrasil.com.br>
2023-08-28 01:03:09 -0300

    fix nontype error

--- crawlers.py
+++ crawlers.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
 class CrawlerTribunal():
     def __init__(self, nome, url):
@@ -17,27 +18,29 @@ class CrawlerTribunal():
             response = requests.get(new_url)
             content = response.content
             soup = BeautifulSoup(content, 'html.parser')
-            vara_processo = (soup.find('span', attrs={'id' : 'varaProcesso'})).text
-            classe = (soup.find('span', attrs={'id' : 'classeProcesso'})).text
-            assunto = (soup.find('span', attrs={'id' : 'assuntoProcesso'})).text
-            area = (soup.find('div', attrs={'id' : 'areaProcesso'})).text
-            juiz = (soup.find('span', attrs={'id' : 'juizProcesso'})).text
-            data_distribuicao = (soup.find('div', attrs={'id' : 'dataHoraDistribuicaoProcesso'})).text
-            valor_acao = (soup.find('div', attrs={'id' : 'valorAcaoProcesso'})).text
-            partes_processo = (soup.find('table', attrs={'id' : 'tablePartesPrincipais'})).text
+            paths = [
+                    'varaProcesso',
+                    'classeProcesso',
+                    'assuntoProcesso',
+                    'areaProcesso',
+                    'juizProcesso',
+                    'dataHoraDistribuicaoProcesso',
+                    'valorAcaoProcesso',
+                    'tablePartesPrincipais'
+                    ]
+            
+            resultados = {} 
 
-            infos = { "numero_processo" : num, 
-                     "vara_processo" : vara_processo,
-                     "classe_processo" : classe,
-                     "assunto_processo" : assunto,
-                     "area_processo" : area,
-                     "juiz_processo": juiz,
-                     "data_distribuicao" : data_distribuicao,
-                     "valor_acao" : valor_acao,
-                     "partes_processo" : partes_processo,
-                     }
+            for path in paths:
+                element = soup.find('span', attrs={'id': path}) or soup.find('div', attrs={'id': path}) or soup.find('table', attrs={'id': path})
+                if element:
+                    resultados[path] = str(element.text.replace("\n", "").replace("\t", "").replace("\xa0", ""))
+                else:
+                    resultados[path] = 'nao consta'
+            
+            pretty = json.dumps(resultados, indent=4)
+            print(pretty)
 
-            print(infos)
         except:
             raise
 
@@ -49,4 +52,4 @@ class CrawlerTribunal():
 
    
 crawler_tjal = CrawlerTribunal('tjal', "https://www2.tjal.jus.br/cpopg/show.do?processo.codigo=[0-9A-Z]{12}&processo.foro=1&processo.numero=")
-crawler_tjal.get_url("0001019-40.2012.8.02.0050")
+crawler_tjal.get_url("0001557-39.2012.8.02.0044")
